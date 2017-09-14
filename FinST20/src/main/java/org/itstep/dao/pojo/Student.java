0b55ffa2200d7c3687e9768fa6b3240f12aee471
Alex2207 <ignatenko2207@gmail.com>
2017-09-14 20:59:31 +0300

    Made test

--- FinST20/src/main/java/org/itstep/dao/pojo/Student.java
+++ FinST20/src/main/java/org/itstep/dao/pojo/Student.java
@@ -2,6 +2,7 @@ package org.itstep.dao.pojo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -11,8 +12,24 @@ import lombok.Setter;
 @Setter
 @Getter
 @Table(name = "STUDENTS")
-public class Student extends User{
+public class Student{
 
+	@Id
+	@Column(name = "LOGIN")
+	private String login;
+	
+	@Column(name = "PASSWORD")
+	private String password;
+	
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+	
+	@Column(name = "LAST_NAME")
+	private String lastName;
+	
 	@Column(name = "GROUP_NAME")
 	private String groupName;
+	
+	public Student() {
+	}
 }

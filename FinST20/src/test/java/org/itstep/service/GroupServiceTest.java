8ac8fa984d7f8e2114951f559e9a511402828405
student <student@K03-01.lan.itstep.kiev.ua>
2017-09-26 20:43:40 +0300

    new test

--- FinST20/src/test/java/org/itstep/service/GroupServiceTest.java
+++ FinST20/src/test/java/org/itstep/service/GroupServiceTest.java
@@ -1,9 +1,7 @@
 package org.itstep.service;
 
 import static org.junit.Assert.*;
-
 import java.util.List;
-
 import org.itstep.App;
 import org.itstep.dao.StudentDAO;
 import org.itstep.dao.pojo.Group;
@@ -16,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
+
 public class GroupServiceTest {
 	
 	@Autowired
@@ -28,28 +27,44 @@ public class GroupServiceTest {
 		group.setGroupName("ST20");
 		Group groupFromDB = groupService.createAndUpdateGroup(group);		
 		assertNotNull(groupFromDB);
-		
+		groupService.deleteGroup(groupFromDB.getGroupName());
 	}
+
 
 	@Test
 	public void testGetGroup() {
-//		Group checkedGroup = groupService.findOne(groupFromDB.getGroupName());
-//		assertNotNull(checkedGroup);
-//		assertEquals("ST20", groupFromDB.getCourse());
-//		groupService.delete(groupFromDB.getGroupName());
+		Group group = new Group();
+		group.setCourse(1);
+		group.setGroupName("ST20");
+		Group groupFromDB = groupService.createAndUpdateGroup(group);
+		Group checkedGroup = groupService.getGroup(groupFromDB.getGroupName());
+		assertNotNull(checkedGroup);
+		assertNotEquals("ST20", groupFromDB.getCourse());
+		groupService.deleteGroup(groupFromDB.getGroupName());
 		
 	}
 
+
+
 	@Test
 	public void testFindAllByCourse() {
-//		List<Group> groupList = groupService.findAllByCourse(1);
-//		assertTrue(!groupList.isEmpty());
-//		groupService.delete(groupFromDB.getGroupName());
+		Group group = new Group();
+		group.setCourse(1);
+		group.setGroupName("ST20");
+		Group groupFromDB = groupService.createAndUpdateGroup(group);
+		List<Group> groupList = groupService.findAllByCourse(1);
+		assertTrue(!groupList.isEmpty());
+		groupService.deleteGroup(groupFromDB.getGroupName());
 	}
+
 
 	@Test
 	public void testDelete() {
-//		groupService.delete(groupFromDB.getGroupName());
+		Group group = new Group();
+		group.setCourse(1);
+		group.setGroupName("ST20");
+		Group groupFromDB = groupService.createAndUpdateGroup(group);
+		groupService.deleteGroup(groupFromDB.getGroupName());
 		
 		
 	}
